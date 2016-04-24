cf37b92d40587aa13e77a7334cbe1adbd349afc5
Artur Burtsev <artjock@gmail.com>
2016-04-24 18:47:42 +0200

    Switching to ! as a default separator

--- test/spec/index.js
+++ test/spec/index.js
@@ -23,26 +23,26 @@ describe('decode', function() {
     });
 
     it('should ignore values with a wrong checksum', function() {
-        crech.decode('f', '%');
-        crech.decode('f%a', '%');
-        crech.decode('f%a%b%f2%', '%');
+        crech.decode('f', '!');
+        crech.decode('f!a', '!');
+        crech.decode('f!a!b!f2!', '!');
         
         expect( this.cb.called ).to.be( false );
     });
 
     [
-        ['f%a%b', {'fa': 0, 'fb': 1}],
-        ['f%a,b%c', {'fa': 0, 'fb': 0, 'fc': 1}],
-        ['f%a,b%c,d', {'fa': 0, 'fb': 0, 'fc': 1, 'fd': 1}],
-        ['f%a%', {'fa': 0}],
-        ['f%%a', {'fa': 1}],
-        ['f%%', {}],
-        ['f1%%%f2%a%b', {'f2a': 0, 'f2b': 1}],
-        ['f1%a%b%f2%c%d', {'f1a': 0, 'f1b': 1, 'f2c': 0, 'f2d': 1}]
+        ['f!a!b', {'fa': 0, 'fb': 1}],
+        ['f!a,b!c', {'fa': 0, 'fb': 0, 'fc': 1}],
+        ['f!a,b!c,d', {'fa': 0, 'fb': 0, 'fc': 1, 'fd': 1}],
+        ['f!a!', {'fa': 0}],
+        ['f!!a', {'fa': 1}],
+        ['f!!', {}],
+        ['f1!!!f2!a!b', {'f2a': 0, 'f2b': 1}],
+        ['f1!a!b!f2!c!d', {'f1a': 0, 'f1b': 1, 'f2c': 0, 'f2d': 1}]
     ].forEach(function(one, i) {
 
         it('should decode "' + one[0] + '"', function() {
-            crech.decode(one[0], '%', this.cb);
+            crech.decode(one[0], '!', this.cb);
             expect( this.parsed ).to.eql( one[1] );
         });
 
